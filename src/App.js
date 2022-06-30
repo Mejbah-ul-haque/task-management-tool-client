04d6987b6e088519ece7dc8fd6fb748e19b4d700
Mejbah <mejba.s@gmail.com>
2022-07-01 00:19:03 +0600

    Set Route and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Completed from './Pages/Completed/Completed';
+import ToDo from './Pages/ToDo/ToDo';
+import Calendar from './Pages/Calendar/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="completed" element={<Completed />} />
+        <Route path="todo" element={<ToDo />} />
+        <Route path="calendar" element={<Calendar />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Completed/Completed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Completed = () => {
+  return (
+    <div>
+      <h2>This is Competed</h2>
+    </div>
+  );
+};
+
+export default Completed;
